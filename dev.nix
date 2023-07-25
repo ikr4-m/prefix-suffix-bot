e416b946f41a45081f42da3709d7de94d72d0cbb
Ikramullah <ikramullahzx@gmail.com>
2023-07-25 15:11:57 +0800

    feat(dev): add command to automate import env

--- dev.nix
+++ dev.nix
@@ -8,6 +8,8 @@ pkgs.mkShell {
     shellHook = ''
         echo "[HOOK] Restore all dotnet package."
         dotnet restore
+        echo "[HOOK] Import environment variable."
+        set -o allexport && source ./.env && set +o allexport
         echo "[HOOK] Done!"
     '';
 }
